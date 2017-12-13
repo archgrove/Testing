bab61b06d6db6a4096d1eacfb90781d9259dba06
Adam Wright <adamw@archgrove.co.uk>
2017-12-13 17:56:32 +0000

    Test

--- test.jsx
+++ test.jsx
@@ -1,3 +1,8 @@
 <foo>
   <bar>'test'</bar>
 </foo>
+
+function WarningButton() {
+  // return React.createElement(CustomButton, {color: 'red'}, null);
+  return <CustomButton color="red">foo's</CustomButton>;
+}
